f98e3fc33bacd07f1c3ea06455d353f0bbd3ceb6
golden252 <gideonoyejide5@gmail.com>
2022-12-08 16:14:00 -0800

    dhdi

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
-#include <studio.h>                                                                                                                      
-/**                                                                                                                                     
- * main - Entry                                                                                                                         
- * Return: Always 0                                                                                                                     
- */ 
+#include <stdio.h>
+/**
+ * Description: main - Prints out texts to stdout.
+ * Return: 0 should the code runs successfully.
+ */
 int main(void)
 {
 		printf("with proper grammar, but the outcome is a piece of art,\n");
