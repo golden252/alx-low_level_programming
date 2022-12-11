acf19e4a751d3f20826e8726167f1a98ea6c09b6
golden252 <gideonoyejide5@gmail.com>
2022-12-11 13:22:27 -0800

    ds

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,21 +4,22 @@
  * Description: main - A program that prints lc and up
  * You can only use the putchar function
  * Return: 0
-*/
+ */
+
 int main(void)
 {
-	int slts = 'a';
-	int caps = 'A';
-	while (slts <= 'z')
-	{
-		putchar(slts);
-		slts += 1;
-	}
-	while (caps <= 'Z')
-	{
-		putchar(caps);
-		caps += 1;
-	}
-	putchar('\n');
-	return (0);
+int slts = 'a';
+int caps = 'A';
+while (slts <= 'z')
+{
+putchar(slts);
+slts += 1;
+}
+while (caps <= 'Z')
+{
+putchar(caps);
+caps += 1;
+}
+putchar('\n');
+return (0);
 }
