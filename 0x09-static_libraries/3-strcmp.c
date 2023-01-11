06dffa7943d95553704e72a94accb3bf4d029e60
golden252 <gideonoyejide5@gmail.com>
2023-01-11 12:15:41 -0800

    face a lot difficuities

--- 0x09-static_libraries/3-strcmp.c
+++ 0x09-static_libraries/3-strcmp.c
@@ -15,4 +15,16 @@ int _strcmp(char *s1, char *s2)
 	while (*(s1 + x) != '\0')
 	{
 		if (*(s1 + x) > *(s2 + x))
-
+		{
+			ltr = *(s1 + x) - *(s2 + x);
+			return (ltr);
+		}
+		else if (*(s1 + x) < *(s2 + x))
+		{
+			ltr = *(s1 + x) - *(s2 + x);
+			return (ltr);
+		}
+		x += 1;
+	}
+	return (0);
+}
