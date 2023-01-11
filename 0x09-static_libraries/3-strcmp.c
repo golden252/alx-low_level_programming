40cc1bf96d492fc1de67050eecb2c3a014f072ab
golden252 <gideonoyejide5@gmail.com>
2023-01-11 02:34:41 -0800

    done

---
+++ 0x09-static_libraries/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strcmp - function that compares two strings.
+ * @s1: pointer to char source 1
+ * @s2: pointer to char source 2
+ * Return: Always 0.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int x = 0;
+	int ltr;
+
+	while (*(s1 + x) != '\0')
+	{
+		if (*(s1 + x) > *(s2 + x))
+
