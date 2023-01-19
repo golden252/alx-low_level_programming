41b4afd42a4f0ed6a56118ab00491e3abcef8f2a
golden252 <gideonoyejide5@gmail.com>
2023-01-19 11:59:15 -0800

    fjd

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,47 +1,25 @@
-#include "3-calc.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#ifndef CALC
+#define CALC
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
+int (*get_op_func(char *s))(int, int);
 
 /**
- * main - a function.
- * @argc: arguments count.
- * @argv: arguments vector.
- * Return: EXIT_SUCCESS.
+ * struct op - Struct op.
+ *
+ * @op: The operator.
+ * @f: The function associated.
  */
-int main(int argc _attribute_((unused)), char **argv)
+
+typedef struct op
 {
-	int a1, b2, x;
+	char *op;
+	int (*f)(int a, int b);
+} op_t;
 
-	op_t oper;
-
-	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-
-	x = strlen(argv[2]);
-
-	if (strcmp(argv[2], "+") != 0
-		&& strcmp(argv[2], "-") != 0
-		&& strcmp(argv[2], "*") != 0
-		&& strcmp(argv[2], "/") != 0
-		&& strcmp(argv[2], "%") != 0)
-	{
-		printf("Error\n");
-		exit(99);
-	}
-
-	if (x > 1)
-	{
-		printf("Error\n");
-		exit(99);
-	}
-	a1 = atoi(argv[1]);
-	b2 = atoi(argv[3]);
-	oper.op = argv[2];
-	oper.f = get_op_func(argv[2]);
-	printf("%d\n", oper.f(a1, b2));
-	return (0);
-}
+#endif
